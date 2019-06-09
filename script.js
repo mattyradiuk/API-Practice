367a4815a89a81876f55d7aa4855b5c1ea2f4460
matt radiuk <mattrdk2@gmail.com>
2019-06-08 17:43:14 -0700

    second commit

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-var request = new XMLHttpRequest()
 
-request.open('GET', 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1', true)
-request.onload = function() {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
+	var request = new XMLHttpRequest()
 
-  if (request.status >= 200 && request.status < 400) {
-      console.log(data)
-	
-  } else {
-    console.log('error')
-  }
+	request.open('GET', 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1', true)
+	request.onload = function() {
+
+	var data = JSON.parse(this.response)
+	var deckid = data.deck_id
+		drawCard(deckid)
+	if (request.status >= 200 && request.status < 400) {
+        console.log(data)	
+  		} else {
+  	  		console.log('error')
+  		}
+	}
+
+	request.send()
+
+
+function drawCard(deckid) {
+	var request = new XMLHttpRequest()
+
+	request.open('GET', 'https://deckofcardsapi.com/api/deck/'+ deckid +'/draw/?count=2', true)
+	request.onload = function() {
+
+	var data = JSON.parse(this.response)
+
+	if (request.status >= 200 && request.status < 400) {
+        console.log(data)	
+  		} else {
+  	  		console.log('error')
+  		}
+	}
+
+	request.send()
 }
-
-request.send()
